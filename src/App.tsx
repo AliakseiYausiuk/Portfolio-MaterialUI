50832c0c4c0584278b38bff18bb3b87ef11139ef
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-09-13 21:25:21 +0300

    feat: add routes pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {Home} from './componets/pages/home/Home';
+import {AboutME} from './componets/pages/aboutME/AboutME';
+import {Portfolio} from './componets/pages/Portfolio';
+import {Contact} from './componets/pages/Contact';
+import {Blog} from './componets/pages/Blog';
 
 
 function App() {
-  return <></>
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/aboutMe" element={<AboutME/>}/>
+            <Route path="/portfolio" element={<Portfolio/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/blog" element={<Blog/>}/>
+        </Routes>
+    </BrowserRouter>
 
 }
 
