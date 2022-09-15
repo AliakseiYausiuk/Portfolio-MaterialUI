65214f360e33c6b83f86d2ac8d1b4a72abba7af1
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-09-15 20:51:12 +0300

    feat: add component Title

---
+++ src/componets/title/Title.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './title.module.scss'
+
+type TitleType = {
+    title?: string
+    titleYellow?: string
+    subtitle?: string
+}
+
+export const Title = ({title, titleYellow, subtitle}:TitleType) => {
+    return (
+        <div className={styles.title__wrapper}>
+            <h1 className={styles.title}>
+                {title}
+                <span className={styles.title__yellow}>{titleYellow}</span>
+                <span className={styles.title__subtitle}>{subtitle}</span>
+            </h1>
+        </div>
+    )
+}
