bd5e2f5d10f91ca6adc6c7682ae94cb3253a952b
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-09-18 16:59:39 +0300

    refactor: change structure of the project

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {Home} from './componets/pages/home/Home';
 import {AboutME} from './componets/pages/aboutME/AboutME';
 import {Portfolio} from './componets/pages/portfolio/Portfolio'
-import {Contact} from './componets/pages/Contact';
-import {Blog} from './componets/pages/Blog';
+import {Contact} from './componets/pages/contact/Contact';
+import {Blog} from './componets/pages/blog/Blog';
 
 
 function App() {
