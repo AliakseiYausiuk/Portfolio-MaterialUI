93b095480cb02bfffa86fde32484d986b27aa717
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-09-26 19:38:04 +0300

    feat: add change color theme

--- src/componets/btn/Button.tsx
+++ src/componets/btn/Button.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moonImg from '../../icons/icons8-moon-48.png'
 
 export type btnType = {
     classNameBtn?: any
@@ -8,7 +9,14 @@ export type btnType = {
 }
 
 export const Button = ({imgUrl, classNameBtn, alt, classNameImg}: btnType) => {
-    return <button className={classNameBtn}>
+
+    const changeTheme = () => {
+        const styl = 'activeTheme';
+        document.body.classList.toggle(styl);
+
+    }
+
+    return <button className={classNameBtn} onClick={changeTheme}>
         <img src={imgUrl} alt={alt} className={classNameImg}/>
     </button>
 }
