d5d316cb4860b4553771910effb5205912a5fa41
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2023-02-06 18:34:23 +0300

    fix:

--- src/componets/navbar/Navbar.tsx
+++ src/componets/navbar/Navbar.tsx
@@ -7,25 +7,23 @@ import iconsPortfolio from '../../icons/icons8-briefcase.svg'
 import iconsContact from '../../icons/icons8-contact-24.png'
 import iconsBlog from '../../icons/icons8-speech-bubble.svg'
 import {PATH} from '../../App';
-import { v1 } from 'uuid';
+import {v1} from 'uuid';
 import {DataNav} from '../../models/models';
-
-
 
 
 export const Navbar = () => {
 
     const dataNav: Array<DataNav> = [
-        {id:v1(), img: iconsHome, alt: "icon Home", text: 'Home',path:PATH.HOME},
-        {id:v1(), img: iconsUser, alt: "icon user", text: 'About',path:PATH.ABOUTME},
-        {id:v1(), img: iconsPortfolio, alt: "icon portfolio", text: 'Portfolio',path:PATH.PORTFOLIO},
-        {id:v1(), img: iconsContact, alt: "icon contact", text: 'Contacnt',path:PATH.CONTACT},
-        {id:v1(), img: iconsBlog, alt: "icon blog", text: 'Blog',path:PATH.BLOG},
+        {id: v1(), img: iconsHome, alt: 'icon Home', text: 'Home', path: PATH.HOME},
+        {id: v1(), img: iconsUser, alt: 'icon user', text: 'About', path: PATH.ABOUTME},
+        {id: v1(), img: iconsPortfolio, alt: 'icon portfolio', text: 'Portfolio', path: PATH.PORTFOLIO},
+        {id: v1(), img: iconsContact, alt: 'icon contact', text: 'Contacnt', path: PATH.CONTACT},
+        {id: v1(), img: iconsBlog, alt: 'icon blog', text: 'Blog', path: PATH.BLOG},
     ]
 
     return (
         <nav className={styles.navbar}>
-            {dataNav.map((el:DataNav) => {
+            {dataNav.map((el: DataNav) => {
                 return <NavLink to={el.path} className={styles.navbar__link} key={el.id}>
                     <img className={styles.navbar__img} src={el.img} alt={el.alt}/>
                     <h3 className={styles.navbar__title}>{el.text}</h3>
